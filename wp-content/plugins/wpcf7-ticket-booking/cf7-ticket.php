452d4dd3be7e40f2f7b666d9a3efd0f88502fc57
zafar iqbal soomro <soomro2021@gmail.com>
2022-06-07 10:37:23 +0500

    sc fix

--- wp-content/plugins/wpcf7-ticket-booking/cf7-ticket.php
+++ wp-content/plugins/wpcf7-ticket-booking/cf7-ticket.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: Contact form 7 Ticket Booking add-on
  * Plugin URI: https://example.com/
@@ -13,47 +14,60 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-class CF7_Ticket_Booking{
-	public function __construct(){
-		add_action( 'plugins_loaded', array( $this, 'tb_load_plugin_textdomain' ) );
+/**
+ * Ticket booking Class
+ */
+class CF7_Ticket_Booking {
+
+	/**
+	 * Load tags and create ticket fields
+	 */
+	public function __construct() {
+		 add_action( 'plugins_loaded', array( $this, 'tb_load_plugin_textdomain' ) );
 		if ( class_exists( 'WPCF7' ) ) {
 			$this->tb_constants();
-			register_activation_hook(  __FILE__, array( &$this, 'tbCreateTable') );
-	
+			register_activation_hook( __FILE__, array( &$this, 'tb_create_table' ) );
+
 			require_once CF_TB_PATH . 'includes/autoload.php';
-		}else{
-			
+		} else {
+
 			add_action( 'admin_notices', array( $this, 'tb_admin_error_notice' ) );
 		}
-		
 	}
-
+	/**
+	 * Plugin text domain
+	 */
 	public function tb_load_plugin_textdomain() {
-
-		load_plugin_textdomain( 'tb-cf7', false, basename( dirname( __FILE__ ) ) . '/languages' ); 
+		load_plugin_textdomain( 'tb-cf7', false, basename( dirname( __FILE__ ) ) . '/languages' );
 	}
-
-	public function tb_admin_error_notice(){
-		$message = sprintf( esc_html__( 'The %1$s Ticket booking add-on Contact Form 7%2$s plugin requires %1$sContact form 7%2$s plugin active to run properly. Please install %1$scontact form 7%2$s and activate', 'tb-cf7' ),'<strong>', '</strong>');
+	/**
+	 * Admin error notice
+	 */
+	public function tb_admin_error_notice() {
+		$message = sprintf( esc_html__( 'The %1$s Ticket booking add-on Contact Form 7%2$s plugin requires %1$sContact form 7%2$s plugin active to run properly. Please install %1$scontact form 7%2$s and activate', 'tb-cf7' ), '<strong>', '</strong>' );
 
 		printf( '<div class="notice notice-error"><p>%1$s</p></div>', wp_kses_post( $message ) );
 	}
-  public function tb_constants(){
-		
+
+	/**
+	 * Plugin path
+	 */
+	public function tb_constants() {
 		if ( ! defined( 'CF_TB_PATH' ) ) {
 			define( 'CF_TB_PATH', plugin_dir_path( __FILE__ ) );
 		}
 		if ( ! defined( 'CF_TB_URL' ) ) {
 			define( 'CF_TB_URL', plugin_dir_url( __FILE__ ) );
 		}
-		
 	}
-
-	public	function tbCreateTable() {
+	/**
+	 * Create ticket table with 100 data of ticket
+	 */
+	public function tb_create_table() {
 		global $wpdb;
 		$charset_collate = $wpdb->get_charset_collate();
-		$table_name = $wpdb->prefix . 'cf7booking';
-		$sql = "CREATE TABLE `$table_name` (
+		$table_name      = $wpdb->prefix . 'cf7booking';
+		$sql             = "CREATE TABLE `$table_name` (
 		`id` int(11) NOT NULL,
 		`status` int(11) DEFAULT '0',
 		`booking_time` datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
@@ -61,25 +75,25 @@ class CF7_Ticket_Booking{
 		PRIMARY KEY(id)
 		) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 		";
-		if ($wpdb->get_var("SHOW TABLES LIKE '$table_name'") != $table_name) {
-		  require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-		  dbDelta($sql);
-
+		if ( $wpdb->get_var( "SHOW TABLES LIKE '$table_name'" ) != $table_name ) {
+			require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+			dbDelta( $sql );
 		}
-		$array = range(1, 10);
+		$array = range( 1, 100 );
 
-        foreach ($array as $ticket) {
-         $wpdb->insert(	$table_name , array(
-            'id' => $ticket,
-         ));
-        }
-
-
-	  }
+		foreach ( $array as $ticket ) {
+			$wpdb->insert(
+				$table_name,
+				array(
+					'id' => $ticket,
+				)
+			);
+		}
+	}
 }
 
 
 
 
 
-$CF7_Ticket_Booking = new CF7_Ticket_Booking();
+$cf7_ticket_booking = new CF7_Ticket_Booking();
